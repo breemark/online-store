9efed8983045351fc77a190964e2ee437d3f0c7f
breemark <britomaximiliano83@gmail.com>
2023-04-03 14:39:35 +0700

    Chapter 7- Index and About Pages

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,5 +4,23 @@ import { Controller, Get, Render } from '@nestjs/common';
 export class AppController {
   @Get("/")
   @Render('index')  // Render the HBS view
-  index() {}        // index method will render the index view.
+  index() {
+    return {
+      title: 'Home Page - Online Store'
+    };
+  }
+  
+  @Get("/about")
+  @Render('about')
+  about(){
+    let viewData = [];
+    viewData["description"] = "This is an about page";
+    viewData["author"] = "Developer by Maximiliano Torres";
+    let data1 = 'About us - Online Store';
+    return {
+      title: data1,
+      subtitle: "About us",
+      viewData: viewData
+    };
+  }
 }
