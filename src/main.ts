a8f01d010355fbd3867219e734bebbb5724ca625
breemark <britomaximiliano83@gmail.com>
2023-04-03 13:59:10 +0700

    Chapter 6- Layout View

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import * as hbs from 'hbs';
+import * as hbsUtils from 'hbs-utils';
 
 async function bootstrap() {
   // const app = await NestFactory.create(AppModule);
@@ -11,6 +13,8 @@ async function bootstrap() {
 
   app.useStaticAssets(join(__dirname, '..', 'public')); //publid dir for storing static assets
   app.setBaseViewsDir(join(__dirname, '..', 'views'));  //views dir will contain templates
+  hbs.registerPartials(join(__dirname, '..', 'views/layouts'));
+  hbsUtils(hbs).registerWatchedPartials(join(__dirname, '..', 'views/layouts'));
   app.setViewEngine('hbs');                             //hbs template engine will be used to render HTML
 
   await app.listen(3000);
