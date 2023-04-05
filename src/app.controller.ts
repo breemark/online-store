fee6fb5ee58862ce1d291bd3d4bdfe8c2f6c7bfc
breemark <britomaximiliano83@gmail.com>
2023-04-05 11:25:56 +0700

    Chapter 9- Use of a Coding Standard-Lint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,27 +2,26 @@ import { Controller, Get, Render } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  @Get("/")
-  @Render('index')  // Render the HBS view
+  @Get('/')
+  @Render('index') // Render the HBS view
   index() {
-    let viewData = [];
-    viewData['title'] = 'Home Page - Online Store'; 
-    return {
-      viewData: viewData
-    };
-  }
-  
-  @Get("/about")
-  @Render('about')
-  about(){
     const viewData = [];
-    viewData['title'] = 'About us - Online Store';
-    viewData['subtitle'] = 'About us';
-    viewData["description"] = "This is an about page";
-    viewData["author"] = "Developer by Maximiliano Torres";
+    viewData['title'] = 'Home Page - Online Store';
     return {
-      viewData: viewData
+      viewData: viewData,
     };
   }
 
+  @Get('/about')
+  @Render('about')
+  about() {
+    const viewData = [];
+    viewData['title'] = 'About us - Online Store';
+    viewData['subtitle'] = 'About us';
+    viewData['description'] = 'This is an about page';
+    viewData['author'] = 'Developer by Maximiliano Torres';
+    return {
+      viewData: viewData,
+    };
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -7,15 +7,13 @@ import * as hbsUtils from 'hbs-utils';
 
 async function bootstrap() {
   // const app = await NestFactory.create(AppModule);
-  const app = await NestFactory.create<NestExpressApplication>(
-    AppModule,
-  ); 
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useStaticAssets(join(__dirname, '..', 'public')); //publid dir for storing static assets
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));  //views dir will contain templates
+  app.setBaseViewsDir(join(__dirname, '..', 'views')); //views dir will contain templates
   hbs.registerPartials(join(__dirname, '..', 'views/layouts'));
   hbsUtils(hbs).registerWatchedPartials(join(__dirname, '..', 'views/layouts'));
-  app.setViewEngine('hbs');                             //hbs template engine will be used to render HTML
+  app.setViewEngine('hbs'); //hbs template engine will be used to render HTML
 
   await app.listen(3000);
 }
