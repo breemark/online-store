101f6a29242f4d6f3c2b2e2b53c1bbc56543105a
breemark <britomaximiliano83@gmail.com>
2023-04-07 11:22:24 +0700

    sqlite database setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { ProductsController } from './products.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: process.env.DATABASE_NAME,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController, ProductsController],
 })
+
 export class AppModule {}
+
+//TODO find a better way to make the TypeORM config. What is the best way to do this?
